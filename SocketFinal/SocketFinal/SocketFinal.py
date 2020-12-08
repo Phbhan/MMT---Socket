ff368f721ed87c491ac87c71b250199efd019a21
19127302 <64116177+19127302@users.noreply.github.com>
2020-12-08 08:08:47 -0800

    Revert "Merge branch 'master' of https://github.com/Phbhan/MMT---Socket"
    This reverts commit 9ba8fbf8ca815f56d5db4a0668969e61367ffd9f, reversing
    changes made to d921d6ce23d3fd3df05b9220e27130f4fa6c084d.

--- SocketFinal/SocketFinal/SocketFinal.py
+++ SocketFinal/SocketFinal/SocketFinal.py
@@ -1,15 +1,15 @@
 import socket
-import os
-# 1. Create a server
+
+# create a server
 
 
 def CreateServer(host, port):
     Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     Server.bind((host, port))
-    Server.listen(1)
+    Server.listen(3)
     return Server
 
-# 3. Read the request from client
+# read the request from client
 
 
 def ReadRequest(Client):
@@ -19,16 +19,15 @@ def ReadRequest(Client):
         re = Client.recv(1024).decode()
         while (re):
             re = re + Client.recv(1024).decode()
-            # use while cause we wnanna read all of the data, the data may be bigger than 1024
+            # use while cause we wanna read all of the data, the data may be bigger than 1024
     except socket.timeout:  # after 1 second of no activity- time out
         if not re:
             print("Didn't receive data! [Timeout]")
     finally:
         return re
 
-# 2. Client connect Server + 3. Read HTTP Request
 
-
+# Read http request
 def ReadHTTPRequest(Server):
     re = ""
     while (re == ""):
@@ -36,8 +35,6 @@ def ReadHTTPRequest(Server):
         print("Client: ", address, " connected with server")
         re = ReadRequest(Client)
     return Client, re
-
-# I/ 4 + 5. Send page index.html
 
 
 def SendFileIndex(Client):
@@ -54,8 +51,6 @@ Content-Length: %d
     header += L.decode()
     Client.send(bytes(header, 'utf-8'))
 
-# I/ 4 + 5. Move the indirect
-
 
 def MovePageIndex(Client):
     header = """HTTP/1.1 301 Moved Permanently
@@ -65,29 +60,35 @@ Location: http://127.0.0.1:8080/index.html
     print("---------------HTTP respone move Index.html: ")
     print(header)
     Client.send(bytes(header, 'utf-8'))
+    Server.close()
 
-# I/ 4. Send HTTP Response  + 5. Close Server
+# 4. Send HTTP Response  + 5. Close Server
 
 
 def MoveHomePage(Server, Client, Request):
-    if "GET /index.html HTTP/1.1" in Request:
+    if "GET / index.html HTTP/1.1" in Request:
         SendFileIndex(Client)
         Server.close()
         return True
+    if "GET / HTTP/1.1" in Request:
+        # Move Index.html
+        MovePageIndex(Client)
+        # Tra ve file index.html cho client
+        Server = CreateServer("localhost", 8080)
+        Client, Request = ReadHTTPRequest(Server)
+        print("------------------HTTP request: ")
+        print(Request)
+        MoveHomePage(Server, Client, Request)
+        return True
+
+
+def checkLogin(request):
+    if "POST / HTTP/1.1" not in Request:
+        return False
+    if "Username=admin&Password=admin" in Request:
+        return True
     else:
-        if "GET / HTTP/1.1" in Request:
-            # Move Index.html
-            MovePageIndex(Client)
-            Server.close()
-            # Tra ve file index.html cho client
-            Server = CreateServer("localhost", 8080)
-            Client, Request = ReadHTTPRequest(Server)
-            print("------------------HTTP request: ")
-            print(Request)
-            MoveHomePage(Server, Client, Request)
-            return True
-        else:
-            return False
+        return False
 
 
 def Move404(Server, Client):
@@ -98,48 +99,43 @@ Location: http://127.0.0.1:8080/404.html
     print("HTTP respone: ")
     print(header)
     Client.send(bytes(header, "utf-8"))
+    Server.close()
 
 
 def SendFile404(Client):
     f = open("404.html", "rb")
     L = f.read()
-    header = """HTTP/1.1 200 OK
+    header = """HTTP/1.1 404 Not Found
 Content-Type: text/html; charset=UTF-8
 Content-Encoding: UTF-8
 Content-Length: %d
-
 """ % len(L)
-    print("-----------------HTTP respone  404.html: ")
+    print("HTTP respone file 404.html: ")
     print(header)
     header += L.decode()
     Client.send(bytes(header, 'utf-8'))
 
-# II/ 4. Send HTTP Response  + 5. Close Server
 
-
-def Move404Page(Server, Client, Request):
-    Move404(Server, Client)
-    Server.close()
-    # 1. Create Server Socket
+def Send404(Server, Client):
+   # Create Server Socket
     Server = CreateServer("localhost", 8080)
-    # 2. Client connect Server + 3. Read HTTP Request
+    # Client connect Server + 3. Read HTTP Request
     Client, Request = ReadHTTPRequest(Server)
-    print("----------------HTTP requset: ")
+    print("----------------HTTP request: ")
     print(Request)
-    SendFile404(Client)
+    if "GET /404.html HTTP/1.1" in Request:
+        SendFile404(Client)
     Server.close()
-
-# Check username and password
 
 
 def MoveInfo(Server, Client):
     header = """HTTP/1.1 301 Moved Permanently
 Location: http://127.0.0.1:8080/info.html
-
 """
     print("HTTP respone: ")
     print(header)
     Client.send(bytes(header, "utf-8"))
+    Server.close()
 
 
 def SendFileInfo(Client):
@@ -149,41 +145,34 @@ def SendFileInfo(Client):
 Content-Type: text/html; charset=UTF-8
 Content-Encoding: UTF-8
 Content-Length: %d
-
 """ % len(L)
-    print("-----------------HTTP respone  404.html: ")
+    print("-----------------HTTP respone  Info.html: ")
     print(header)
     header += L.decode()
     Client.send(bytes(header, 'utf-8'))
 
 
 def SendImg(Client, NameImg):
-    f = open(NameImg, "rb")
-    L = f.read()
-    header = """HTTP/1.1 200 OK
+    with open(NameImg, 'rb') as f:
+        L = f.read()
+        header = """HTTP/1.1 200 OK
 Content-Type: text/html; charset=UTF-8
 Content-Encoding: UTF-8
 Content-Length: %d
 """ % len(L)
-    print("-----------------HTTP respone: ")
-    print(header)
-    header = bytes(header, 'utf-8') + L
-    Client.send(header)
+        print("-----------------HTTP respone: ")
+        print(header)
+        header = bytes(header, 'utf-8') + L
+        Client.send(header)
 
 
-# II/ 4. Send HTTP Response  + 5. Close Server
-
-
-def MoveInfoPage(Server, Client, Request):
-    MoveInfo(Server, Client)
-    Server.close()
-    # 1. Create Server Socket
+def SendInfo(Server, Client):
     Server = CreateServer("localhost", 8080)
-    # 2. Client connect Server + 3. Read HTTP Request
     Client, Request = ReadHTTPRequest(Server)
-    print("----------------HTTP requset: ")
+    print("HTTP Request: ")
     print(Request)
-    SendFileInfo(Client)
+    if "GET /info.html HTTP/1.1" in Request:
+        SendFileInfo(Client)
     Server.close()
     # image 1
     Server = CreateServer("localhost", 8080)
@@ -205,36 +194,29 @@ def MoveInfoPage(Server, Client, Request):
     Server.close()
 
 
-def checkLogin(Request):
-    if "Username=admin&Password=admin" in Request:
-        return True
-    else:
-        return False
-
-
-# Main function
+# main function
 if __name__ == "__main__":
     while True:
         print("Part 1: return homepage when access")
-        # 1. Create Server Socket
+        # Create Server Socket
         Server = CreateServer("localhost", 8080)
-        # 2. Client connect Server + 3. Read HTTP Request
+        # Client connect Server + 3. Read HTTP Request
         Client, Request = ReadHTTPRequest(Server)
-        print("----------------HTTP requset: ")
+        print("----------------HTTP request: ")
         print(Request)
-        # 4. Send HTTP Rea + 5. Close Sever
-        if MoveHomePage(Server, Client, Request) == True:
-            print("Part 2: login")
-            # 1. Create Server Socket
-            Server = CreateServer("localhost", 8080)
-            # 2. Client connect Server + 3. Read HTTP Request
-            Client, Request = ReadHTTPRequest(Server)
-            print("----------------HTTP requset: ")
-            print(Request)
-            # 4. Send HTTP Rea + 5. Close Sever
-            if checkLogin(Request) == True:
-                MoveInfoPage(Server, Client, Request)
-            else:
-                Move404Page(Server, Client, Request)
+        # Send HTTP Rea + Close Sever
+        MoveHomePage(Server, Client, Request)
+        print("Part 2: login")
+        # Create Server Socket
+        Server = CreateServer("localhost", 8080)
+        # Client connect Server + 3. Read HTTP Request
+        Client, Request = ReadHTTPRequest(Server)
+        print("----------------HTTP request: ")
+        print(Request)
+        # Send HTTP Rea + Close Sever
+        if checkLogin(Request) == True:
+            MoveInfo(Server, Client)
+            SendInfo(Server, Client)
         else:
-            Server.close()
+            Move404(Server, Client)
+            Send404(Server, Client)
